0915300c8d35c0a6de473bad35b05b7305074af4
Cameron Chardukian <camchardukian@gmail.com>
2021-08-17 19:29:41 +0700

    feature/secondary-player-chips

---
+++ src/components/SecondaryPlayerSection/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.scss'
+
+const SecondaryPlayerSection = ({ chipsRemaining = [], onSelectChip }) => {
+    return (
+        <div className="secondary-section-container">
+            <div className="player-chips-container">
+                {chipsRemaining.map((chip) => {
+                    return (
+                        <div className="player-chip" key={chip}>
+                            {chip}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default SecondaryPlayerSection
