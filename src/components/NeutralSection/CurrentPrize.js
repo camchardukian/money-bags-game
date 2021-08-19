1ef1827e9e8e8884bd52a89cb528cb3c8a66aefd
Cameron Chardukian <camchardukian@gmail.com>
2021-08-19 19:45:30 +0700

    feature/active-chips

--- src/components/NeutralSection/CurrentPrize.js
+++ src/components/NeutralSection/CurrentPrize.js
@@ -1,9 +1,12 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react'
+import './styles.scss'
 const CurrentPrize = ({ prize = 0 }) => {
-  return (
-    <div className="current-prize-container">CurrentPrize is: {prize}</div>
-  );
-};
+    return (
+        <div className="current-prize-container">
+            <div className="current-prize-text">Current Prize</div>
+            <div className="neutral-chip">{prize}</div>
+        </div>
+    )
+}
 
-export default CurrentPrize;
+export default CurrentPrize
--- src/components/NeutralSection/PrizesRemaining.js
+++ src/components/NeutralSection/PrizesRemaining.js
@@ -1,14 +1,22 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react'
+import './styles.scss'
 const PrizesRemaining = ({ prizesRemainingArray = [] }) => {
-  return (
-    <div className="prizes-remaining-container">
-      Prizes Remaining are:
-      {prizesRemainingArray.map(number => {
-        return <div key={number}>{number}</div>;
-      })}
-    </div>
-  );
-};
+    return (
+        <div className="prizes-remaining-container">
+            <div className="prizes-remaining-title">Prizes Remaining</div>
+            <div className="remaining-neutral-chips-list">
+                {prizesRemainingArray.map((number) => {
+                    return (
+                        <div className="remaining-neutral-chip-container">
+                            <div className="neutral-chip" key={number}>
+                                {number}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
 
-export default PrizesRemaining;
+export default PrizesRemaining
